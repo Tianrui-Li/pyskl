2f06b2df25b7a38e4ef38a2489ad7520bdebdd06
Tianrui Li <litianrui1998@gmail.com>
2023-07-15 20:54:04 +0200

    add transformer

--- configs/transformer/j2.py
+++ configs/transformer/j2.py
@@ -66,7 +66,7 @@ log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
 
 # runtime settings
 log_level = 'INFO'
-work_dir = './work_dirs/transformer/j2'
+work_dir = './work_dirs/transformer/j2/7.15'
 
 auto_resume = False
 seed = 88
