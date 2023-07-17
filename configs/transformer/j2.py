c835b6c54b546b226808e5f4ce34ab828bed4c5c
Tianrui Li <litianrui1998@gmail.com>
2023-07-18 00:09:33 +0200

    add transformer

--- configs/transformer/j2.py
+++ configs/transformer/j2.py
@@ -49,28 +49,28 @@ data = dict(
     videos_per_gpu=24,
     workers_per_gpu=8,
     test_dataloader=dict(videos_per_gpu=1),
-    # train=dict(
-    #     type='RepeatDataset',
-    #     times=2,
-    #     dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train')),
-    train=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train'),
+    train=dict(
+        type='RepeatDataset',
+        times=2,
+        dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train')),
+    # train=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train'),
     val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='xsub_val'),
     test=dict(type=dataset_type, ann_file=ann_file, pipeline=test_pipeline, split='xsub_val'))
 
 # optimizer
-optimizer = dict(type='Adam', lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005, amsgrad=False)
-# optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005, nesterov=True)
+# optimizer = dict(type='Adam', lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005, amsgrad=False)
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 120
+lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=True)
+total_epochs = 60
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy'])
 log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
 
 # runtime settings
 log_level = 'INFO'
-work_dir = './work_dirs/transformer/j2/7.19-tm-6'
+work_dir = './work_dirs/transformer/j2/7.19-tm-7'
 
 auto_resume = False
 seed = 88
