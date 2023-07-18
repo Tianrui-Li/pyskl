e805d1709bfbf57839951ea12146205a20d36de9
Tianrui Li <litianrui1998@gmail.com>
2023-07-18 18:37:02 +0200

    add transformer

--- configs/transformer/j2.py
+++ configs/transformer/j2.py
@@ -18,7 +18,7 @@ ann_file = 'data/nturgbd/ntu60_3danno.pkl'
 clip_len = 100
 train_pipeline = [
     dict(type='PreNormalize3D'),
-    dict(type='RandomScale', scale=0.2),
+    dict(type='RandomScale', scale=0.1),
     dict(type='RandomRot'),
     # dict(type='RandomRot', theta=0.2),
     dict(type='GenSkeFeat', dataset='nturgb+d', feats=['j']),
@@ -52,7 +52,7 @@ data = dict(
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
         type='RepeatDataset',
-        times=5,
+        times=2,
         dataset=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train')),
     # train=dict(type=dataset_type, ann_file=ann_file, pipeline=train_pipeline, split='xsub_train'),
     val=dict(type=dataset_type, ann_file=ann_file, pipeline=val_pipeline, split='xsub_val'),
@@ -60,18 +60,18 @@ data = dict(
 
 # optimizer
 # optimizer = dict(type='Adam', lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005, amsgrad=False)
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005, nesterov=True)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005, nesterov=True)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 48
+total_epochs = 60
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy'])
 log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
 
 # runtime settings
 log_level = 'INFO'
-work_dir = './work_dirs/transformer/j2/7.19-tm-10'
+work_dir = './work_dirs/transformer/j2/7.19-tm-11'
 
 auto_resume = False
 seed = 88
