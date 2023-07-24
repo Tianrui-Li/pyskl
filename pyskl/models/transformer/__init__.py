e1c93f4f39f0b14548b4dbdac5ab9397e9f5161f
Tianrui Li <litianrui1998@gmail.com>
2023-07-24 20:24:06 +0200

    add transformer

--- pyskl/models/transformer/__init__.py
+++ pyskl/models/transformer/__init__.py
@@ -2,3 +2,4 @@ from .transformer import ViViT1  # noqa: F401, F403
 from .transformer import ViViT2  # noqa: F401, F403
 from .vivit3 import ViViT3  # noqa: F401, F403
 from .vivit4 import ViViT4  # noqa: F401, F403
+from .vivit1n import ViViT1n  # noqa: F401, F403
