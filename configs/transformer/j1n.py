972446f003ac339ba671fec8306679ebd34ecb41
Tianrui Li <litianrui1998@gmail.com>
2023-08-02 02:26:00 +0200

    add transformer

--- configs/transformer/j1n.py
+++ configs/transformer/j1n.py
@@ -6,14 +6,16 @@ model = dict(
     backbone=dict(
         type='ViViT1n',
         graph_cfg=dict(layout='nturgb+d', mode='spatial'),
-        max_position_embeddings=1001,  # 32*25+1=801，
+        max_position_embeddings_1=401,  # 96/6*25+1=401
+        max_position_embeddings_2=7,
+        # T*V+1 32*25+1=801，
         dim=256,
     ),
     cls_head=dict(type='vit2Head', num_classes=60, in_channels=256))
 
 dataset_type = 'PoseDataset'
 ann_file = 'data/nturgbd/ntu60_3danno.pkl'
-clip_len = 100
+clip_len = 96
 train_pipeline = [
     dict(type='PreNormalize3D'),
     dict(type='RandomScale', scale=0.1),
