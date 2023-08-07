2059f84e12ba309387f18acf192558369a08cef4
Tianrui Li <litianrui1998@gmail.com>
2023-08-07 13:41:43 +0200

    add transformer

--- configs/transformer/j3.py
+++ configs/transformer/j3.py
@@ -44,7 +44,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['keypoint'])
 ]
 data = dict(
-    videos_per_gpu=16,
+    videos_per_gpu=24,
     workers_per_gpu=8,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
