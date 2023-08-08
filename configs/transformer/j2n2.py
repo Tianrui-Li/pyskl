e9d233334d965252268bb9bf9cc8d43da6630ca0
Tianrui Li <litianrui1998@gmail.com>
2023-08-08 23:18:39 +0200

    add transformer

--- configs/transformer/j2n2.py
+++ configs/transformer/j2n2.py
@@ -71,7 +71,7 @@ log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
 
 # runtime settings
 log_level = 'INFO'
-work_dir = './work_dirs/transformer/j2/8.8-tm2-2'
+work_dir = './work_dirs/transformer/j2/8.8-tm2-4'
 
 auto_resume = False
 seed = 88
