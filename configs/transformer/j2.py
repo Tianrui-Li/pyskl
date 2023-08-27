7fd3cd58a972fe28ad5fb19f2293c086e4328cc2
tianrui <litianrui1998@gmail.com>
2023-08-27 21:39:30 +0200

    add Transformer

--- configs/transformer/j2.py
+++ configs/transformer/j2.py
@@ -48,7 +48,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['keypoint'])
 ]
 data = dict(
-    videos_per_gpu=24,
+    videos_per_gpu=16,
     workers_per_gpu=8,
     test_dataloader=dict(videos_per_gpu=1),
     train=dict(
