5d1e4e36b59524c3d51ce33cfbd71e8c481f66bb
tianrui <litianrui1998@gmail.com>
2023-10-06 19:25:44 +0200

    vivit2-add new Transformer

--- configs/transformer/j2n2.py
+++ configs/transformer/j2n2.py
@@ -51,7 +51,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['keypoint'])
 ]
 data = dict(
-    videos_per_gpu=32,
+    videos_per_gpu=64,
     workers_per_gpu=8,
     test_dataloader=dict(videos_per_gpu=1),
     # train=dict(
