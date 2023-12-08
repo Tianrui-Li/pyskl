67dd4c8e06bef0f4347f68fb2dc3a1513f697a5f
tianrui <litianrui1998@gmail.com>
2023-12-08 19:22:16 +0100

    change initial function

--- pyskl/models/transformer/ST_JT.py
+++ pyskl/models/transformer/ST_JT.py
@@ -1,7 +1,6 @@
 
 import torch
-import math
-from torch import nn, einsum
+from torch import nn
 from einops import rearrange, pack
 from ..builder import BACKBONES
 from ..gcns import unit_tcn
@@ -66,6 +65,7 @@ class Attention(nn.Module):
         super().__init__()
         self.dim = dim
 
+        # dim的变化
         if dim == 64:
             self.group_size = (64, 25)
             self.num_heads = 2
@@ -78,6 +78,7 @@ class Attention(nn.Module):
         elif dim == 512:
             self.group_size = (8, 25)
             self.num_heads = 16
+
         # self.num_heads = num_heads
 
         head_dim = dim // num_heads
@@ -198,164 +199,12 @@ class TransformerEncoderLayer(nn.Module):
         return src
 
 
-def sliding_window_attention_mask(
-        seq_len, window_size, neighborhood_size, dtype, with_cls=True):
-    """
-    Generate sliding window attention mask with a quadratic window shape.
-
-    Args:
-    - seq_len: The total sequence length
-    - window_size: Size of the attention window
-    - neighborhood_size: Number of neighboring windows around the central window
-
-    Returns:
-    - mask: A seq_len x seq_len mask where 0 indicates positions that can be attended to, and -1e9 (or a very large negative value) indicates positions that should not be attended to.
-    """
-
-    def fn(seq_len, window_size, neighborhood_size):
-        mask = torch.full((seq_len, seq_len), torch.finfo(dtype).min)
-
-        num_chunks = math.ceil(seq_len / window_size)
-        q_chunks = torch.arange(seq_len).chunk(num_chunks)
-        for i, q_chunk in enumerate(q_chunks):
-            q_start, q_end = q_chunk[0], q_chunk[-1] + 1
-            k_start = max(0, (i - neighborhood_size) * window_size)
-            k_end = min(seq_len, (i + neighborhood_size + 1) * window_size)
-            mask[q_start: q_end, k_start: k_end] = 0
-
-        return mask
-
-    if not with_cls:
-        return fn(seq_len, window_size, neighborhood_size)
-
-    orig_len = seq_len
-    seq_len = seq_len - 1
-    # mask without considering cls token
-    mask = fn(seq_len, window_size, neighborhood_size)
-    mask = torch.cat([torch.full((seq_len, 1), torch.finfo(dtype).min), mask], dim=1)
-    mask = torch.cat([torch.zeros(1, orig_len, dtype=dtype), mask], dim=0)
-    return mask
-
-
-# def weights_init(m):
-#     classname = m.__class__.__name__
-#     if classname.find('Conv') != -1:
-#         if hasattr(m, 'weight'):
-#             nn.init.kaiming_normal_(m.weight, mode='fan_out')
-#         if hasattr(m, 'bias') and m.bias is not None and isinstance(m.bias, torch.Tensor):
-#             nn.init.constant_(m.bias, 0)
-#     elif classname.find('BatchNorm') != -1:
-#         if hasattr(m, 'weight') and m.weight is not None:
-#             m.weight.data.normal_(1.0, 0.02)
-#         if hasattr(m, 'bias') and m.bias is not None:
-#             m.bias.data.fill_(0)
-
-
-class TemporalConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1):
-        super(TemporalConv, self).__init__()
-        pad = (kernel_size + (kernel_size - 1) * (dilation - 1) - 1) // 2
-        self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=(kernel_size, 1),
-            padding=(pad, 0),
-            stride=(stride, 1),
-            dilation=(dilation, 1),
-        )
-
-        self.bn = nn.BatchNorm2d(out_channels)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        return x
-
-
-class MultiScale_TemporalConv(nn.Module):
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size=3,
-                 stride=1,
-                 dilations=[1, 2, 3, 4],
-                 residual=False,
-                 residual_kernel_size=1):
-
-        super().__init__()
-        assert out_channels % (len(dilations) + 2) == 0, '# out channels should be multiples of # branches'
-
-        # Multiple branches of temporal convolution
-        self.num_branches = len(dilations) + 2
-        branch_channels = out_channels // self.num_branches
-        if type(kernel_size) == list:
-            assert len(kernel_size) == len(dilations)
-        else:
-            kernel_size = [kernel_size] * len(dilations)
-        # Temporal Convolution branches
-        self.branches = nn.ModuleList([
-            nn.Sequential(
-                nn.Conv2d(
-                    in_channels,
-                    branch_channels,
-                    kernel_size=1,
-                    padding=0
-                ),
-                nn.BatchNorm2d(branch_channels),
-                nn.ReLU(inplace=True),
-                TemporalConv(
-                    branch_channels,
-                    branch_channels,
-                    kernel_size=ks,
-                    stride=stride,
-                    dilation=dilation
-                ),
-            )
-            for ks, dilation in zip(kernel_size, dilations)
-        ])
-
-        # Additional Max & 1x1 branch
-        self.branches.append(nn.Sequential(
-            nn.Conv2d(in_channels, branch_channels, kernel_size=1, padding=0),
-            nn.BatchNorm2d(branch_channels),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=(3, 1), stride=(stride, 1), padding=(1, 0)),
-            nn.BatchNorm2d(branch_channels)  # 为什么还要加bn
-        ))
-
-        self.branches.append(nn.Sequential(
-            nn.Conv2d(in_channels, branch_channels, kernel_size=1, padding=0, stride=(stride, 1)),
-            nn.BatchNorm2d(branch_channels)
-        ))
-
-        # Residual connection
-        if not residual:
-            self.residual = lambda x: 0
-        elif (in_channels == out_channels) and (stride == 1):
-            self.residual = lambda x: x
-        else:
-            self.residual = TemporalConv(in_channels, out_channels, kernel_size=residual_kernel_size, stride=stride)
-        # print(len(self.branches))
-        # initialize
-        self.apply(weights_init)
-
-    def forward(self, x):
-        # Input dim: (N,C,T,V)
-        res = self.residual(x)
-        branch_outs = []
-        for tempconv in self.branches:
-            out = tempconv(x)
-            branch_outs.append(out)
-
-        out = torch.cat(branch_outs, dim=1)
-        out += res
-        return out
-
-
 class TemporalPooling(nn.Module):
     def __init__(self, dim_in, dim_out, kernel_size, stride, pooling=False,
-                 with_cls=True, dilations=[1, 2]):
+                 with_cls=True, dilations=None):
         super().__init__()
+        if dilations is None:
+            dilations = [1, 2]
         self.pooling = pooling
         self.with_cls = with_cls
         if dim_in == dim_out:
@@ -364,10 +213,6 @@ class TemporalPooling(nn.Module):
             if with_cls:
                 self.cls_mapping = nn.Linear(dim_in, dim_out)
             self.temporal_pool = unit_tcn(dim_in, dim_out, kernel_size, stride)
-            # self.temporal_pool = MultiScale_TemporalConv(dim_in, dim_out, kernel_size=kernel_size, stride=stride,
-            #                                              dilations=dilations,
-            #                                              #  residual=True has worse performance in the end
-            #                                              residual=False)
         else:
             self.temporal_pool = nn.Linear(dim_in, dim_out)
 
@@ -399,7 +244,6 @@ class TemporalPooling(nn.Module):
 class ST_JT(nn.Module):
     """Locality-aware Spatial-Temporal Transformer
     """
-
     def __init__(
             self,
             # graph_cfg,
@@ -411,15 +255,11 @@ class ST_JT(nn.Module):
             num_heads=4,
             mlp_ratio=4,
             norm_first=False,
-            activation='relu',
             attention_dropout=0.1,
             stochastic_depth_rate=0.1,
-            dropout=0.1,
             dropout_rate=0.,
             use_cls=False,
             layer_norm_eps=1e-6,
-            max_joints=25,
-            max_frames=100,
             temporal_pooling=True,
             sliding_window=False,
             stride1=2,
@@ -442,10 +282,6 @@ class ST_JT(nn.Module):
                           if use_cls else None)
         self.pos_embed_cls = (nn.Parameter(torch.zeros(1, 1, hidden_dim))
                               if use_cls else None)
-
-        # # We use two embeddings, one for joints and one for frames
-        # self.joint_pe = PositionalEncoding(hidden_dim, max_joints)
-        # self.frame_pe = PositionalEncoding(hidden_dim, max_frames)
 
         # Variable hidden dim
         hidden_dims = []
@@ -471,34 +307,40 @@ class ST_JT(nn.Module):
                 TemporalPooling(
                     dim_in, dim_out, kernel_size1, stride1,
                     pooling=temporal_pooling, with_cls=use_cls),
-                # # Transformer encoder
-                # nn.TransformerEncoderLayer(
-                #     dim_out, num_heads, dim_out * mlp_ratio, dropout,
-                #     activation, layer_norm_eps, batch_first=True,
-                #     norm_first=norm_first)
                 TransformerEncoderLayer(d_model=dim_out, nhead=num_heads,
                                         dim_feedforward=dim_out * mlp_ratio, dropout=dropout_rate,
                                         attention_dropout=attention_dropout, drop_path_rate=next(dpr_iter))
-
             ]))
-
-        # Variable locality-aware mask
-        if sliding_window:
-            nb_size = 0
-            self.neighborhood_sizes = [nb_size]
-            for dim_in, dim_out in hidden_dims[1:]:
-                nb_size = nb_size + (dim_in == dim_out)
-                self.neighborhood_sizes.append(nb_size)
 
         self.norm = (nn.LayerNorm(512, eps=layer_norm_eps)
                      if norm_first else None)
 
         self.init_weights()
 
+    # def init_weights(self):
+    #     for p in self.parameters():
+    #         if p.dim() > 1:
+    #             nn.init.xavier_uniform_(p)
+
     def init_weights(self):
-        for p in self.parameters():
-            if p.dim() > 1:
-                nn.init.xavier_uniform_(p)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if hasattr(m, 'weight'):
+                    nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if hasattr(m, 'bias') and m.bias is not None and isinstance(m.bias, torch.Tensor):
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                if hasattr(m, 'weight') and m.weight is not None:
+                    m.weight.data.normal_(1.0, 0.02)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    m.bias.data.fill_(0)
+            elif isinstance(m, nn.Linear):
+                nn.init.trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
 
     def forward(self, x):
         N, M, T, V, C = x.size()
@@ -512,13 +354,6 @@ class ST_JT(nn.Module):
         # embed the inputs, orig dim -> hidden dim
         x_embd = self.embd_layer(x)
 
-        # # add positional embeddings
-        # x_input = self.joint_pe(x_embd)  # joint-wise
-        # x_input = self.frame_pe(rearrange(x_input, 'b t v c -> b v t c'))  # frame wise
-        # # convert to required dim order
-        # x_input = rearrange(x_input, 'b v t c -> b (t v) c')
-
-        # 旋转位置编码
         x_input = rearrange(x_embd, 'b t v c -> b (t v) c')
 
         # prepend the cls token for source if needed
@@ -533,15 +368,7 @@ class ST_JT(nn.Module):
             # Temporal pooling
             hidden_state = temporal_pool(hidden_state, v=V)
 
-            # Construct attention mask if required
-            if self.sliding_window:
-                attn_mask = sliding_window_attention_mask(
-                    seq_len=hidden_state.size(1),
-                    window_size=V,
-                    neighborhood_size=self.neighborhood_sizes[i],
-                    dtype=hidden_state.dtype,
-                    with_cls=self.use_cls,
-                ).to(hidden_state.device)
+            # encoder
             hidden_state = encoder(hidden_state, attn_mask)
 
         if self.norm is not None:
