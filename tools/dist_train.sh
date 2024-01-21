21a0b4f6a5d022200e4eae274fa55e9354d271a3
tianrui <litianrui1998@gmail.com>
2024-01-21 08:21:04 +0100

    ST_JT_org GPU 3

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -2,7 +2,7 @@
 
 export MASTER_PORT=$((12000 + $RANDOM % 20000))
 # for server run
- export CUDA_VISIBLE_DEVICES=0
+ export CUDA_VISIBLE_DEVICES=3
 # export CUDA_VISIBLE_DEVICES=1,2
 # export CUDA_VISIBLE_DEVICES=4,5
 
